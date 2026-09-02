2361a2649c681ff5d6e7727d01ecf98e17c007de
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2026-09-02 16:24:09 +0330

    feat: adding font

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,10 @@
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
-  )
-}
+    <section className="text-center mt-20 text-2xl">
+      <h1 className="font-kaghaz">عنوان با فونت کاغذ</h1>
+      <p className="font-vazir">متن با فونت وزیر</p>
+    </section>
+  );
+};
 
-export default Homepage
+export default Homepage;
