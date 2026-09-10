03af1c7cc6532f4507981b16d9208228220e3aeb
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2026-09-10 22:17:28 +0330

    feat: add CartPage component to display shopping cart

---
+++ client/src/app/cart/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Container from "../components/container";
+
+function CartPage() {
+  return (
+    <section>
+      <Container>
+        <p>سبد خرید</p>
+      </Container>
+    </section>
+  );
+}
+
+export default CartPage;
