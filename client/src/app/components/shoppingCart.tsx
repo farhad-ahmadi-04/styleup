32cbe39a041a9a09129cc4d8709b3dabd174c57d
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2026-09-17 19:23:29 +0330

    feat: update ShoppingCart component to display item count from cart store

--- client/src/app/components/shoppingCart.tsx
+++ client/src/app/components/shoppingCart.tsx
@@ -1,12 +1,18 @@
+"use client";
+
+import useCartStore from "@/stores/cartStore";
 import { ShoppingCartIcon } from "lucide-react";
 import Link from "next/link";
 
 function ShoppingCart() {
+  const { cart, hasHydrated } = useCartStore();
+
+  if (!hasHydrated) return null;
   return (
     <Link href={"/cart"} className="relative">
       <ShoppingCartIcon className="w-4 h-4  text-gray-600" />
       <span className="absolute bg-amber-400 rounded-full  w-4 h-4 text-gray-600 flex items-center justify-center -top-3 left-2 font-medium text-xs">
-        0
+        {cart.reduce((acc, item) => acc + item.quantity, 0)}
       </span>
     </Link>
   );
